65a271973802a1e20d68ff4192f68e40ce76e5c2
west2 <west2@localhost.localdomain>
2023-09-20 13:57:09 +0800

    修复无法ping成功的问题

--- load_config.py
+++ load_config.py
@@ -25,19 +25,25 @@ def connect_test(pc1, pc2):
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         # getpass.getpass() 用于隐藏输入的密码
-        getpassword = getpass.getpass(f"请输入 {pc1.ip} 的密码: ")
-        logging.info('请输入 %s 的密码:',{pc1.ip} , extra={'logfile': log_file})
 
+        getpassword = getpass.getpass(f"请输入 {pc2['ip']} 的密码: ")
+        logging.info('请输入 %s 的密码:',{pc2['ip']} , extra={'logfile': log_file})
         # 默认端口号为22
-        ssh.connect(hostname=pc1.ip, port=22, username=pc1.user, password=getpassword)
+        ssh.connect(hostname=pc2['ip'], port=22, username=pc2['user'], password=getpassword)
+        logging.info('%s 与 %s 连通性测试成功', {pc1['ip']}, {pc2['ip']}, extra={'logfile': log_file})
+        print(f"{pc1['ip']} 与 {pc2['ip']} 连通性测试成功")
         ssh.close()
-        logging.info('%s 与 %s 连通性测试成功', {pc1.ip}, {pc2.ip}, extra={'logfile': log_file})
-        print(f"{pc1.ip} 与 {pc2.ip} 连通性测试成功")
+    except paramiko.AuthenticationException:
+        logging.error('%s 与 %s 连通性测试失败：认证失败', pc1['ip'], pc2['ip'], extra={'logfile': log_file})
+        print(f"{pc1['ip']} 与 {pc2['ip']} 连通性测试失败：0 认证失败")
+    except paramiko.SSHException as e:
+        logging.error('%s 与 %s 连通性测试失败：%s', pc1['ip'], pc2['ip'], str(e), extra={'logfile': log_file})
+        print(f"{pc1['ip']} 与 {pc2['ip']} 连通性测试失败：1 {str(e)}")
     except Exception as e:
-        logging.error('%s 与 %s 连通性测试失败', {pc1.ip}, {pc2.ip}, extra={'logfile': log_file})
-        print(f"{pc1.ip} 与 {pc2.ip} 连通性测试失败")
-        print(e)
-
+        logging.error('%s 与 %s 连通性测试失败：%s', pc1['ip'], pc2['ip'], str(e), extra={'logfile': log_file})
+        print(f"{pc1['ip']} 与 {pc2['ip']} 连通性测试失败：2 {str(e)}")
+    finally:
+        ssh.close()
 
 # host_test模式: 3台主机
 def host_test_body(data) :
@@ -92,8 +98,8 @@ def host_test_body(data) :
 # communication_test模式: 2台主机 
 def communication_test_body(data):
 
-    local = {'ip': '', 'user': ''}
-    remote = {'ip': '', 'user': ''}
+    local = data['communication_test']['local']
+    remote = data['communication_test']['remote']
     if not data["communication_test"]["local"]["ip"]:
         logging.info('本机IP缺失', extra={'logfile': log_file})
         local['ip'] = input("请输入本机IP, 或者直接编辑config.json文件: ")
@@ -106,9 +112,12 @@ def communication_test_body(data):
     if not data["communication_test"]["remote"]["user"]:
         logging.info('远程主机用户名缺失', extra={'logfile': log_file})
         remote['user'] = input("请输入远程主机用户名, 或者直接编辑config.json文件: ")
-    try:
-        connect_test(local, remote)
-        logging.info('本机与远程主机连通性测试成功', extra={'logfile': log_file})
-    except Exception as e:
-        logging.info('本机与远程主机连通性测试失败', extra={'logfile': log_file})
-        print("本机与远程主机连通性测试失败")
+    print("本机IP: ", local['ip'])
+    print("本机用户名: ", local['user'])
+    print("远程主机IP: ", remote['ip'])
+    print("远程主机用户名: ", remote['user'])
+    connect_test(local, remote)
+    logging.info('本机与远程主机连通性测试成功', extra={'logfile': log_file})
+    #except Exception as e:
+    #    logging.info('本机与远程主机连通性测试失败', extra={'logfile': log_file})
+    #    print("本机与远程主机连通性测试失败")
